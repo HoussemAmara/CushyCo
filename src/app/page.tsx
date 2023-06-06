48c4dfc50e9154f79141e7b97a00aa52f9a526e8
Houssem AMARA <houssemamara35@outlook.fr>
2023-06-07 01:35:14 +0200

    Commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
 import { Hero } from "./components";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>CushyCo</title>
-      </Head>
       <Hero></Hero>
     </>
   );
